f85cffd6a84a724969919dd1056b49ec0705e5a1
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-12-07 23:19:53 +0000

    feat: next config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
 };
-
-export default nextConfig;
