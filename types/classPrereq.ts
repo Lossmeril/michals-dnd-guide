73021dd88ad4d9812c1786e95ac576381791d8ff
Michal Špitálský <michal.spitalsky@pm.me>
2026-02-03 16:32:38 +0100

    refactor: fresh start

--- types/classPrereq.ts
+++
@@ -1,4 +0,0 @@
-export type ClassPrerequisiteRow = {
-  child_class_id: string;
-  parent_class_id: string;
-};
--- types/shared.ts
+++
@@ -1,6 +0,0 @@
-export function isOneOf<T extends readonly string[]>(
-  value: string,
-  options: T,
-): value is T[number] {
-  return options.includes(value);
-}
